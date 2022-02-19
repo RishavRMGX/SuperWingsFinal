0e140f39c6124f151bbecd58fde7eef947ca1469
RishavRMGX <rishav.mishra@rmgx.in>
2022-02-19 16:45:07 +0530

    Fix

--- src/main/java/com/superwings/assignment/domain/PeriodicTable.java
+++ src/main/java/com/superwings/assignment/domain/PeriodicTable.java
@@ -23,4 +23,36 @@ public class PeriodicTable {
 
     @Column(name = "atomic_weight", nullable = false)
     private Double atomicWeight;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getChemicalSymbol() {
+        return chemicalSymbol;
+    }
+
+    public void setChemicalSymbol(String chemicalSymbol) {
+        this.chemicalSymbol = chemicalSymbol;
+    }
+
+    public String getChemicalName() {
+        return chemicalName;
+    }
+
+    public void setChemicalName(String chemicalName) {
+        this.chemicalName = chemicalName;
+    }
+
+    public Double getAtomicWeight() {
+        return atomicWeight;
+    }
+
+    public void setAtomicWeight(Double atomicWeight) {
+        this.atomicWeight = atomicWeight;
+    }
 }
